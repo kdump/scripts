577f02db5043001b6cdb66bde689a68cac59d83b
可达书院 <acheng@kdump.cn>
2013-04-12 15:19:57 +0800

    Update HE_IPv6_tunnel_update.sh
    instead of put (stale) settings in /etc/network/interfaces and update it later, it's better not to use it, just configure the tunnel dynamically when we can determine IPv4 address of the host

--- HE_IPv6_tunnel_update.sh
+++ HE_IPv6_tunnel_update.sh
@@ -1,37 +1,48 @@
-#!/bin/sh
-
-
-########  NOTE:  DO NOT USE YET. IT'S A WORK IN PROGRESS  #######
-
-
-
-#####   Purpose of this script:  To update tunnelbroker.net free IPv6 tunnels
-#####        for those who use dynamic IPv4 (like ADSL) for Internet
-#####        This script also update client side tunnel interface using his/her new IPv4 addr
+#!/bin/bash
 
 #######  HE tunnel broker account info  ##########
-#######  update them according to your info  #####
-tunnel_user=username
-tunnel_pass=password
-tunnel_id1=xxxxxx
+tunnel_user=user_name
+tunnel_pass=Password00
+tunnel_id=xxxxxx
+
+# =====  udate the following according to your HE tunnel info =========
+ipv6_addr="2001:470:c:FFFF::2"
+ipv6_prefix=64
+ipv6_gateway="2001:470:c:FFFF::1"
+remote_end_ip=xx.xx.xx.xx
+
+####  the interface used to access internet. Default to ppp0 
+####   as this script is meant for ADSL users
+ext_int=ppp0
+
+### name of the virutal tunnel interface. change it to what you like
+tunnel_int="he-ipv6"
 
 #######  tunnel update URL
 #######  according to https://www.tunnelbroker.net/forums/index.php?topic=1994.0
 #######  update it in case of any change
-tunnel_update_url="https://${tunnel_user}:${tunnel_pass}@ipv4.tunnelbroker.net/nic/update?hostname=${tunnel_id1}"
+tunnel_update_url="https://${tunnel_user}:${tunnel_pass}@ipv4.tunnelbroker.net/nic/update?hostname=${tunnel_id}"
 
-####  external_if should point to the interface 
-####      that has a public IPv4 address or the
-####      used to access internet
+### get IPv4 address assigned to $ext_int
+local_ip=$(ip -f inet addr show $ext_int | grep -i inet | awk '{print $2}')
 
-external_if=ppp0
-file_2_update="/etc/network/interfaces"
+####  TO BE DONE #####
+####   verify $local_ip is in right format
 
 ########    not used for now ############
 #distro=
 #OS=
 
+if [ ! -z $local_ip ]
+then
+  echo "   updating HE ipv6 tunnel end point ..."
+	ip tunnel add $tunnel_int mode sit remote $remote_end_ip local $local_ip ttl 255
+	ip link set $tunnel_int up
+	ip addr add ${ipv6_addr}/${ipv6_prefix}  dev ${tunnel_int}
+	route add --inet6 default gw $ipv6_gateway
+	curl $tunnel_update_url 2>&1 >> /dev/null
 
-external_ip=$(ip -f inet addr show  $external_if | grep inet | awk '{print $2}')
+	echo "   ....... DONE"
+fi
 
-## to be continued ...
+### if necessary, update /etc/resolv.conf and use Ipv6 capable DNS servers
